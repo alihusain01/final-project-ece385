fd30e41031b891dea675460085eecdcf75a5cac8
Ali Husain <alijhusain1125@gmail.com>
2022-04-13 19:52:29 -0500

    Beginning project

---
+++ lab61soc/lab61soc_bb.v
@@ -0,0 +1,42 @@
+
+module lab61soc (
+	buttons_external_connection_export,
+	clk_clk,
+	reset_reset_n,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n,
+	keycode_export,
+	usb_irq_export,
+	usb_gpx_export,
+	usb_rst_export,
+	hex_digits_export,
+	leds_export,
+	key_external_connection_export);	
+
+	input	[1:0]	buttons_external_connection_export;
+	input		clk_clk;
+	input		reset_reset_n;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output		sdram_wire_cs_n;
+	inout	[15:0]	sdram_wire_dq;
+	output	[1:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+	output	[7:0]	keycode_export;
+	input		usb_irq_export;
+	input		usb_gpx_export;
+	output		usb_rst_export;
+	output	[15:0]	hex_digits_export;
+	output	[13:0]	leds_export;
+	input	[1:0]	key_external_connection_export;
+endmodule
